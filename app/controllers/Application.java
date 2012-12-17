31e389701f34c41ff65e943cab379c1a2c7fe35e
marioosh <marioosh.net@gmail.com>
2012-12-17 23:17:37 +0100

    lokalne repo z gdata

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,27 @@
 package controllers;
 
+import com.google.gdata.client.photos.PicasawebService;
+import com.google.gdata.util.AuthenticationException;
 import play.*;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
-	/*
-	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+	static private PicasawebService myService;
+	static {
+		try {
+			myService = new PicasawebService("testApp");			
+			myService.setUserCredentials("waclaw.bezimienny@gmail.com", "b7FEvW4mSy9C");
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}		
 	}
-	*/
-
+	
 	public static Result index(String name) {
+
+		Logger.info(Play.application().path()+"");
+		Logger.info(myService+"");
 		if(name == null) {
 			return ok("Hello World");
 		}
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,17 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "com.google.gdata" % "gdata-core" % "1.0",
+      "com.google.gdata" % "gdata-photos" % "2.0",
+      "com.google.gdata" % "gdata-photos-meta" % "2.0",
+      "com.google.gdata" % "gdata-media" % "1.0",
+      "javax.mail" % "mail" % "1.4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
+      // resolvers += "my" at "file://"+baseDirectory(_ / "repo")
+      resolvers += "my" at "file:///home/marioosh/git/play-starter/repo"
     )
 
 }
