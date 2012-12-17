77730cce3ece66bf8c1cf676ef5c0c51e712b464
marioosh <marioosh.net@gmail.com>
2012-12-17 23:30:31 +0100

    lista albumow

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import com.google.gdata.client.photos.PicasawebService;
+import com.google.gdata.data.photos.UserFeed;
 import com.google.gdata.util.AuthenticationException;
+import com.google.gdata.util.ServiceException;
 import play.*;
 import play.mvc.*;
 import views.html.*;
@@ -18,14 +23,21 @@ public class Application extends Controller {
 		}		
 	}
 	
-	public static Result index(String name) {
+	public static Result index(String name) throws IOException, ServiceException {
+		Logger.info(myService+"");
+		Logger.info(name);
+		URL feedUrl = new URL("https://picasaweb.google.com/data/feed/api/user/default?kind=album");
+		UserFeed myUserFeed = myService.getFeed(feedUrl, UserFeed.class);
+		return ok(albums.render(myUserFeed.getAlbumEntries()));
 
+		/*
 		Logger.info(Play.application().path()+"");
 		Logger.info(myService+"");
 		if(name == null) {
 			return ok("Hello World");
 		}
 		return ok("Hello "+name);
+		*/
 	}
 
 }
