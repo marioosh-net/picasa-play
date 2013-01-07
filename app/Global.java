e083011c837d634a2b5b3dcb8e30a6a6cc212608
marioosh <marioosh.net@gmail.com>
2013-01-07 07:47:49 +0100

    Token expired exception handling

--- app/Global.java
+++ app/Global.java
@@ -1,51 +1,16 @@
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Properties;
-import com.google.gdata.client.photos.PicasawebService;
+import static play.mvc.Results.notFound;
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
-import play.mvc.Result;
 import play.mvc.Http.RequestHeader;
-import static play.Logger.error;
-import static play.Logger.info;
-import static play.mvc.Results.*;
+import play.mvc.Result;
 
 public class Global extends GlobalSettings {
 
 	@Override
 	public void onStart(Application app) {
-		
-		try {
-			info("Loading services...");
-			Properties p = new Properties();
-			InputStream in =  Application.class.getResourceAsStream("/resources/accounts.properties");
-			if(in != null) {
-				p.load(in);
-				in.close();
-			
-				Enumeration e = p.propertyNames();
-				List<String[]> l = new ArrayList<String[]>();
-				while(e.hasMoreElements()) {
-					String k = (String) e.nextElement();
-					l.add(new String[]{k+"", p.getProperty(k)});
-					PicasawebService myService = new PicasawebService("testApp");			
-					myService.setUserCredentials(k+"", p.getProperty(k));
-					controllers.Application.myServices.add(myService);
-				}
-			
-			} else {
-				error("null inputstream");
-			}
-		
-			Logger.info("Application has started");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-		
+		controllers.Application.loadServices();
+		Logger.info("Application has started");		
 	}
 
 	@Override
@@ -67,4 +32,5 @@ public class Global extends GlobalSettings {
 	public Result onBadRequest(RequestHeader paramRequestHeader, String paramString) {
 		return notFound("Page Not Found");
 	}
+
 }
