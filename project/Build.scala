87a44714391e952d9469f8f19a38c22471cb7ae2
marioosh <marioosh.net@gmail.com>
2013-03-12 15:15:25 +0100

    migration to play 2.1.0; some errors yet

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
-import PlayProject._
+import play.Project._
 import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
@@ -21,7 +20,7 @@ object ApplicationBuild extends Build {
       "org.apache.httpcomponents" % "httpclient" % "4.2.3"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       resolvers <+= baseDirectory { base => 
         "my" at "file:///"+base.getAbsolutePath+"/repo"
       },
